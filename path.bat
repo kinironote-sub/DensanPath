e1c734e5fa8d22acb623e1378d8445854f087493
Kinironote <ishowta.jp@gmail.com>
2016-11-21 12:44:57 +0900

    fix タブを含んでいると上手く動かなかったのでgotoを使ってタブを無くした

--- path.bat
+++ path.bat
@@ -1,56 +1,61 @@
 @echo off
 if not "%COMPUTERNAME:PCROOM_=%" == "%COMPUTERNAME%" (
-	rem fden
-	echo ‘æˆê“dZº‹y‚Ñ‘æ“ñ“dZº—p‚ÌƒXƒNƒŠƒvƒg‚ğÀs‚µ‚Ü‚·B
-	echo python‚ğƒCƒ“ƒXƒg[ƒ‹‚·‚é‚½‚ß‚Éƒlƒbƒgƒ[ƒNÚ‘±‚ª•K—v‚Å‚·Bƒlƒbƒgƒ[ƒN”FØ‚ğs‚Á‚Ä‚­‚¾‚³‚¢B
-	start http://www.yahoo.co.jp/
-	pause
-	echo;
-	cd C:\users\Public\Downloads
-
-	echo Download python...
-	@powershell -NoProfile -ExecutionPolicy Bypass -Command "$d=new-object System.Net.WebClient;$d.Proxy.Credentials=[System.Net.CredentialCache]::DefaultNetworkCredentials;$d.DownloadFile('https://www.python.org/ftp/python/3.4.4/python-3.4.4.amd64.msi','python.msi')"
-	
-	echo Install python...
-	rem (command of install : http://python.readthedocs.io/en/latest/using/windows.html)
-	msiexec /i python.msi /qn
-	
-	echo Set path to python...
-	setx PATH C:\Python34;C:\Python34\Scripts
-	
-	echo;
-	echo I—¹B
+	goto FDEN
 ) else (
-	rem jden
-	echo J“dZ—p‚ÌƒXƒNƒŠƒvƒg‚ğÀs‚µ‚Ü‚·B
-	echo;
-
-	rem ‘Ã‹¦BŒ³path“à‚Éu"v‚ÅƒGƒXƒP[ƒvo—ˆ‚È‚¢“Áê•¶š‚ªŠÜ‚Ü‚ê‚é‚Æ€‚ÊB
-	rem get pass
-	for /F "tokens=1,2,*" %%a in ('REG QUERY HKEY_CURRENT_USER\Environment /v "Path"') do (set old_path="%%c")
-	set new_path=%old_path:"=%
-
-	set compile_path=
-
-	rem path to java
-	set compile_path="%compile_path%;C:\PROGRA~1\Java\jdk1.8.0_77\bin"
-	set compile_path=%compile_path:"=%
-
-	rem path to c
-	set compile_path="%compile_path%;C:\cygwin64\bin"
-	set compile_path=%compile_path:"=%
-
-	rem path to vc++
-	set compile_path="%compile_path%;C:\Program Files (x86)\Microsoft SDKs\Windows\v7.1A\Lib\x64;C:\Program Files (x86)\Microsoft Visual Studio 14.0\Common7\IDE;C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\bin"
-	set compile_path=%compile_path:"=%
-	rem ’DINCLUDE‚ÆLIB‚Í‚ß‚ñ‚Ç‚¢‚Ì‚Åã‘‚«‚µ‚Ä‚éBŒ»“_‚Å‚Í‘¶İ‚µ‚Ä‚¢‚È‚¢‚Ì‚Å–â‘è‚È‚µ
-	setx INCLUDE "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\include;C:\Program Files (x86)\Windows Kits\10\Include\10.0.10240.0\ucrt"
-	setx LIB "C:\Program Files (x86)\Microsoft Visual Studio 14.0\Common7\IDE;C:\Program Files (x86)\Microsoft SDKs\Windows\v7.1A\Lib\x86;C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\lib;C:\Program Files (x86)\Windows Kits\10\Lib\10.0.10240.0\ucrt\x86;C:\Program Files (x86)\Windows Kits\8.1\Lib\winv6.3\um\x86"
-
-	rem path to python
-	set compile_path="%compile_path%;C:\Users\TeamET\AppData\Local\Programs\Python\Python35-32;C:\Users\TeamET\AppData\Local\Programs\Python\Python35-32\Scripts"
-	set compile_path=%compile_path:"=%
-
-	setx path "%new_path%;%compile_path%"
-
+	goto JDEN
 )
+
+:FDEN
+rem fden
+echo ç¬¬ä¸€é›»ç®—å®¤åŠã³ç¬¬äºŒé›»ç®—å®¤ç”¨ã®ã‚¹ã‚¯ãƒªãƒ—ãƒˆã‚’å®Ÿè¡Œã—ã¾ã™ã€‚
+echo pythonã‚’ã‚¤ãƒ³ã‚¹ãƒˆãƒ¼ãƒ«ã™ã‚‹ãŸã‚ã«ãƒãƒƒãƒˆãƒ¯ãƒ¼ã‚¯æ¥ç¶šãŒå¿…è¦ã§ã™ã€‚ãƒãƒƒãƒˆãƒ¯ãƒ¼ã‚¯èªè¨¼ã‚’è¡Œã£ã¦ãã ã•ã„ã€‚
+start http://www.yahoo.co.jp/
+pause
+echo;
+cd C:\users\Public\Downloads
+
+echo Download python...
+@powershell -NoProfile -ExecutionPolicy Bypass -Command "$d=new-object System.Net.WebClient;$d.Proxy.Credentials=[System.Net.CredentialCache]::DefaultNetworkCredentials;$d.DownloadFile('https://www.python.org/ftp/python/3.4.4/python-3.4.4.amd64.msi','python.msi')"
+
+echo Install python...
+rem (command of install : http://python.readthedocs.io/en/latest/using/windows.html)
+msiexec /i python.msi /qn
+
+echo Set path to python...
+setx PATH C:\Python34;C:\Python34\Scripts
+
+echo;
+echo çµ‚äº†ã€‚
+
+:JDEN
+rem jden
+echo Jé›»ç®—ç”¨ã®ã‚¹ã‚¯ãƒªãƒ—ãƒˆã‚’å®Ÿè¡Œã—ã¾ã™ã€‚
+echo;
+
+rem å¦¥å”ã€‚å…ƒpathå†…ã«ã€Œ"ã€ã§ã‚¨ã‚¹ã‚±ãƒ¼ãƒ—å‡ºæ¥ãªã„ç‰¹æ®Šæ–‡å­—ãŒå«ã¾ã‚Œã‚‹ã¨æ­»ã¬ã€‚
+rem get pass
+for /F "tokens=1,2,*" %%a in ('REG QUERY HKEY_CURRENT_USER\Environment /v "Path"') do (set old_path="%%c")
+set new_path=%old_path:"=%
+
+set compile_path=
+
+rem path to java
+set compile_path="%compile_path%;C:\PROGRA~1\Java\jdk1.8.0_77\bin"
+set compile_path=%compile_path:"=%
+
+rem path to c
+set compile_path="%compile_path%;C:\cygwin64\bin"
+set compile_path=%compile_path:"=%
+
+rem path to vc++
+set compile_path="%compile_path%;C:\Program Files (x86)\Microsoft SDKs\Windows\v7.1A\Lib\x64;C:\Program Files (x86)\Microsoft Visual Studio 14.0\Common7\IDE;C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\bin"
+set compile_path=%compile_path:"=%
+rem æ³¨ï¼INCLUDEã¨LIBã¯ã‚ã‚“ã©ã„ã®ã§ä¸Šæ›¸ãã—ã¦ã‚‹ã€‚ç¾æ™‚ç‚¹ã§ã¯å­˜åœ¨ã—ã¦ã„ãªã„ã®ã§å•é¡Œãªã—
+setx INCLUDE "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\include;C:\Program Files (x86)\Windows Kits\10\Include\10.0.10240.0\ucrt"
+setx LIB "C:\Program Files (x86)\Microsoft Visual Studio 14.0\Common7\IDE;C:\Program Files (x86)\Microsoft SDKs\Windows\v7.1A\Lib\x86;C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\lib;C:\Program Files (x86)\Windows Kits\10\Lib\10.0.10240.0\ucrt\x86;C:\Program Files (x86)\Windows Kits\8.1\Lib\winv6.3\um\x86"
+
+rem path to python
+set compile_path="%compile_path%;C:\Users\TeamET\AppData\Local\Programs\Python\Python35-32;C:\Users\TeamET\AppData\Local\Programs\Python\Python35-32\Scripts"
+set compile_path=%compile_path:"=%
+
+setx path "%new_path%;%compile_path%"
